ce07f9322e4c3d6182c0bb30ba88a7893e434cf7
pasqualedimaro <pakydm15@gmail.com>
2025-07-03 12:20:25 +0200

    fix

--- middleware/errorMiddleware.js
+++ middleware/errorMiddleware.js
@@ -1,11 +1,12 @@
 // creo un middleware per le rotte non registrate
 
 const notFound = (req, res, next) => {
-    res.status(404).json({
+    res.status(404)
+    res.json({
         error: "not found",
         message: "pagina non trovata"
 
-    })
-}
+    });
+};
 
 module.exports = {notFound}
